72ace84829a941e6e8e5659842cbf295d19a3dc6
Jonathan Luk <jluk87@gmail.com>
2018-04-17 01:48:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,11 +5,14 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |k,v|
+      self.send("#{k}=", "#{v}")
+    end
+    self.class.all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|student_hash| Student.new(student_hash)}
   end
 
   def add_student_attributes(attributes_hash)
@@ -17,7 +20,7 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
 end
 
