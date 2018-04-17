86966113322ac6b53b63c2af7c6d86268b8dbf25
Jonathan Luk <jluk87@gmail.com>
2018-04-17 01:51:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,11 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each do |k,v|
-      self.send("#{k}=", "#{v}")
-    end
+    student_hash.each {|k,v| self.send("#{k}=", "#{v}")}
     self.class.all << self
   end
 
@@ -16,11 +14,10 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do
   end
 
   def self.all
     @@all
   end
 end
-
